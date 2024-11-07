e34ac681f58a272867ddff19551031cfa13dec4f
akbarshohhack <akbarshohqurbonboyev01@gmail.com>
2024-11-08 03:15:33 +0500

    Create table.php

---
+++ table.php
@@ -0,0 +1,42 @@
+<!-- templates/table.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma'lumotlar Jadvali</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Age</th>
+    </tr>
+
+    <?php foreach ($data as $row): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($row['id']); ?></td>
+            <td><?php echo htmlspecialchars($row['name']); ?></td>
+            <td><?php echo htmlspecialchars($row['age']); ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
+</body>
+</html>
